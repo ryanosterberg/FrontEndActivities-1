7295751acceb0d7ef50d7e45cc24ad4ed9b0a9ef
Evan Bagwell <etb@wi.rr.com>
2020-06-18 14:19:24 -0500

    Added javaScript

--- CTWMasterClass-FrontEndActivities/CTWMasterClass-FrontEndActivities/Bagwell/mySite.js
+++ CTWMasterClass-FrontEndActivities/CTWMasterClass-FrontEndActivities/Bagwell/mySite.js
@@ -1,1 +1,9 @@
-﻿
+﻿function showHide() {
+    var x = document.getElementById("vision");
+    if (x.style.display !== "none") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
